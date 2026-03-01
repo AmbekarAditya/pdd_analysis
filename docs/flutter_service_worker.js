05ffeec1661f3619d6c1d0ea5d5fe5d9b26d2f90
ADITYA AMBEKAR <aditya.s.ambekar@gmail.com>
2026-03-01 16:15:35 +0530

    chore(deploy): update production build with sub-reasons and adaptive input

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7ce80ff2db0d8f8012cb616d16d397f7",
+const RESOURCES = {"flutter_bootstrap.js": "af4f08508b770dd1da03fb743857e3fa",
 "version.json": "4f3389d17b5b18e8a256207c6296bb4b",
 "index.html": "efdcef0006cfdaa9b623b79291e8c566",
 "/": "efdcef0006cfdaa9b623b79291e8c566",
-"main.dart.js": "7a3b480198f18cf6626b765477379eb1",
+"main.dart.js": "ecd4557ab295f55ef7cef46324dcffba",
 "sqlite3.wasm": "2e9fc1ccbb9d15199fccf405b0ceee53",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
